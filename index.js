34acd1d992bbd63b6479f5789ccba474d6e6539e
sonictrain <nicola.brucoli92@gmail.com>
2024-02-07 00:12:25 +0000

    code refactoring after inquirer and fs testing

--- index.js
+++ index.js
@@ -1,101 +1,161 @@
 const fs = require("fs");
-// const path = require('path');
 const inquirer = require("inquirer");
-// const chalk = require("chalk");
-// const TableInput = require("inquirer-table-input");
-// const generateMarkdown = require("./utils/generateMarkdown");
+// // const path = require('path');
+// // const chalk = require("chalk");
+// // const TableInput = require("inquirer-table-input");
+// // const generateMarkdown = require("./utils/generateMarkdown");
 
+// readme global configuration object
 let readmeConfig = {
-        project: '',
-        description: '',
-        license: '',
+        sections: [],
+        repoLink: '',
+        liveLink: ''
     }
 
-const initReadme = [
-    {
+// init function - readmeConfig builder
+async function init() {
+    getConsent()
+}
+
+// getConsent from user
+async function getConsent() {
+    const consent = {
         type: 'confirm',
         message: 'Do you want to generate a README?',
-        name: 'consent',
-        default: true
-    },
-    {
-        type: 'input',
-        message: `What's the name of your project?`,
-        name: 'projectTitle',
-        when: (answer) => answer.consent === true
-    },
-    {
-        type: 'list',
-        message: `Choose a License for your project from the list below:`,
-        name: 'license',
-        choices: ['Apache License 2.0', 'GNU General Public License', 'MIT License', 'Creative Commons Zero v1.0'],
-        when: (answer) => answer.consent === true
+        name: 'readmeConsent',
+        default: true,
     }
-]
-
-async function addSection2conf() {
-    
-    let needSection = true;
-    
-    const addSections = [
-        {
-            type: 'input',
-            message: 'Please input the section title',
-            name: 'sectionTitle'
-        },
-        {
-            type: 'confirm',
-            message: 'Do you want to another section to the README?',
-            name: 'needSection',
-            default: true
-        }
-    ]
-
-    while (needSection) {
-        await inquirer
-        .prompt(addSections)
-        .then((answers) => {
-            needSection = answers.needSection
-        })
-    }
-
-}
-
-async function init() {
-    const dayjs = require('dayjs')
-
-    const date = dayjs()
     
     await inquirer
-        .prompt(initReadme)
-        .then((answers) => {
-
-            const title = answers.projectTitle;
-            // TODO Destructure the readmeConfig Obj and the answersObj if necessary
-            readmeConfig.project = title
-            readmeConfig.license = answers.license
-
-            const folderTitle = answers.projectTitle.toLowerCase().replace(' ', '_');
-            const folderPath = `./output/${date.format('YYYYMMDD')}/${date.format('HHmmss')}-${folderTitle}`
-
-            fs.mkdirSync(folderPath,
-                { recursive: true },
-                (err) => {
-                    if (err) throw err;
-                }
-                );
-            
-            fs.writeFileSync(
-                `${folderPath}/README.md`,
-                `# ${title}`,
-                (err) =>
-                    err ? console.error(err) : console.log(`README Saved in ${folderPath}!`)
-                )
-            
-            console.log(JSON.stringify(readmeConfig));
+    .prompt(consent)
+    .then((answer) => {
+        console.log(answer.readmeConsent)
+        if (answer.readmeConsent) {
+            getMainSections()
         }
-        )
-    addSection2conf();
+    })
+    .catch((err) => console.error(err))
 }
 
+// launch the initialisation process
 init();
+
+// const initReadme = [
+//     {
+//         type: 'confirm',
+//         message: 'Do you want to generate a README?',
+//         name: 'consent',
+//         default: true
+//     },
+//     {
+//         type: 'input',
+//         message: `What's the name of your project?`,
+//         name: 'projectTitle',
+//         when: (answer) => answer.consent === true
+//     },
+//     {
+//         type: 'input',
+//         message: `Please send me the link to the project's repository`,
+//         name: 'repoLink',
+//         when: (answer) => answer.consent === true
+//     },
+//     {
+//         type: 'input',
+//         message: `Please send me the link to the live app, if any`,
+//         name: 'liveLink',
+//         when: (answer) => answer.consent === true
+//     },
+//     {
+//         type: 'list',
+//         message: `Choose a License for your project from the list below:`,
+//         name: 'license',
+//         // TODO get license list from Github API
+//         choices: [
+//             'Apache License 2.0',
+//             'GNU General Public License',
+//             'MIT License',
+//             'Creative Commons Zero v1.0',
+//             'BSD 2-Clause "Simplified" License',
+//             'BSD 3-Clause "New" or "Revised" License',
+//             'Boost Software License 1.0',
+//             'Eclipse Public License 2.0',
+//             'GNU Affero General Public License v3.0',
+//             'GNU General Public License v2.0',
+//             'GNU Lesser General Public License v2.1',
+//             'Mozilla Public License 2.0',
+//             'The Unlicense'
+//         ],
+//         when: (answer) => answer.consent === true
+//     }
+// ]
+
+// async function addExtraSections() {
+    
+//     let needSection = true;
+//     const extraSections = [
+//         {
+//             type: 'input',
+//             message: 'Please input the section title',
+//             name: 'sectionTitle'
+//         },
+//         {
+//             type: 'confirm',
+//             message: 'Do you want to another section to the README?',
+//             name: 'needSection',
+//             default: true
+//         }
+//     ]
+//     while (needSection) {
+//         await inquirer
+//         .prompt(extraSections)
+//         .then((answers) => {
+//             needSection = answers.needSection
+//         })
+//     }
+// }
+
+// const mainSections = []
+
+
+//     const dayjs = require('dayjs')
+
+//     const date = dayjs()
+//     await inquirer
+//         .prompt(initReadme)
+//         .then((answers) => {
+
+//             const title = answers.projectTitle;
+//             // TODO Destructure the readmeConfig Obj and the answersObj too if necessary
+//             readmeConfig.sections.projectTitle = title
+//             readmeConfig.license = answers.license
+//             readmeConfig.repoLink = answers.repoLink
+//             readmeConfig.liveLink = answers.liveLink
+
+//             const folderTitle = title.toLowerCase().replace(' ', '_');
+//             const folderPath = `./output/${date.format('YYYYMMDD')}/${date.format('HHmmss')}-${folderTitle}`
+
+//             fs.mkdirSync(folderPath,
+//                 { recursive: true },
+//                 (err) => {
+//                     if (err) throw err;
+//                 }
+//                 );
+
+//             fs.writeFileSync(
+//                 `${folderPath}/README.md`,
+//                 `# ${title}`,
+//                 (err) =>
+//                     err ? console.error(err) : console.log(`README Saved in ${folderPath}!`)
+//                 );
+
+//             fs.writeFileSync(
+//                 `${folderPath}/conf.json`,
+//                 `${JSON.stringify(readmeConfig, null, 4)}`,
+//                 (err) =>
+//                     err ? console.error(err) : console.log(`Configuration file saved in ${folderPath}!`)
+//                 );
+
+//             console.log(JSON.stringify(readmeConfig));
+//         }
+//         )
+//     addExtraSections()
