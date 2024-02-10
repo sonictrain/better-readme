5205b918ae0de050eaf9f7208174ae82a7b2a5ba
sonictrain <nicola.brucoli92@gmail.com>
2024-02-10 15:34:39 +0000

    added function to get the license description. improved generator function and added fs method to create folder and related files

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,58 @@
+const fs = require("fs");
+const dayjs = require('dayjs')
+
 // generate and return Markdown
 const generateMarkdown = (readmeConfig) => {
-
-  const { attribution, tableOfContents, sections, user, badges } = readmeConfig
+  const { projectTitle, sections, badges, attribution, questionsSection } = readmeConfig
 
   let content = [...sections.map((s) => {
-    if (s.isTitle) {
-      return `# ${s.bodyContent}
+    if (s.isTitle)
+    {
+return `# ${s.bodyContent}
 ${generateBadgesMD(badges)}\n
-${attachTableOfContents(readmeConfig).join(' ')}\n
-`
-    } else if (s.isMedia) {
-      return `## ${s.sectionName}
-![${s.sectionName}](${s.bodyContent})\n
-`
-    } else {
-      return `## ${s.sectionName}
-${s.bodyContent}\n
-`}
-  }), questionSection(readmeConfig), attributionBR(attribution)]
-  return content.join(' ')
+${attachTableOfContents(readmeConfig).join(' ')}\n`
+    }
+    else if (s.isMedia)
+    {
+return `## ${s.sectionName}
+![${s.sectionName}](${s.bodyContent})\n`
+    }
+    else if (s.isLicense) {
+return `${questionsSection ? getQuestionsSection(readmeConfig) : ''}
+## ${s.sectionName}
+${getLicenseDescription(s)}\n`
+    }
+    else
+    {
+return `## ${s.sectionName}
+${s.bodyContent}\n`
+    }
+  }), attributionBR(attribution)].join(' ')
+
+  const date = dayjs()
+
+  const folderTitle = projectTitle.toLowerCase().split(" ").join("_");
+  const versionFolderPath = `./output/${folderTitle}/${date.format('YYYYMMDD')}`
+
+  fs.mkdirSync(versionFolderPath,
+      { recursive: true },
+      (err) => {
+          if (err) throw err;
+      }
+  );
+
+  fs.writeFileSync(
+    `${versionFolderPath}/${date.format('HHmmss')}-README.md`,
+    `${content}`,
+    (err) => err ? console.error(err) : console.log(`README Succesfully saved in ${versionFolderPath}!`)
+  );
+
+  fs.writeFileSync(
+    `${versionFolderPath}/${date.format('HHmmss')}-conf.json`,
+    `${JSON.stringify(readmeConfig, null, 4)}`,
+    (err) => err ? console.error(err) : console.log(`Configuration file saved in ${versionFolderPath}!`)
+  );
+
 }
 
 // build Shield.io badges component
@@ -36,24 +70,37 @@ const generateBadgesMD = (badgesArr) => {
 const attachTableOfContents = (readmeConfig) => {
 
   const { tableOfContents, sections } = readmeConfig
-  
+
   if (tableOfContents) {
-    return sections.map((s) => {
-      return `- [${s.sectionName}](#${s.sectionName})\n`
+    return sections.map((s, i) => {
+      return `${i}. [${s.sectionName}](#${s.sectionName})\n`
     })
   }
 }
 
 // create question section
-const questionSection = (readmeConfig) => {
-  return `## Questions
-If you have any questions or feedback, please feel fre to reach out to the developer sending an email to the provided [email address](${readmeConfig.user.email}) or [visiting the GitHub profile](http://github.com/${readmeConfig.user.username}/). Your inquiries and input are valuable, appreciate your engagement with the project.\n`
+const getQuestionsSection = (readmeConfig) => {
+  const { user } = readmeConfig
+  return `## Got any questions?
+If you have any questions or feedback, please feel fre to reach out sending an email to the provided [email address](${user.email}) or [visiting the GitHub profile](http://github.com/${user.username}/). Your inquiries and input are valuable, appreciate your engagement with the project!\n`
+}
+
+const getLicenseDescription = async (licenseName) => {
+  try {
+    const res = await fetch(`https://api.github.com/licenses/${licenseName}`)
+    if (res.status === 200) {
+        licenseData = await res.json()
+        return licenseData.description
+    }
+  } catch {
+    (err) => console.error(err)
+  }
 }
 
 // create attribution
 const attributionBR = (attribution) => {
   if (attribution) {
-    return `---\n
+    return `\n---\n
 Generate with [Better Readme](https://github.com/sonictrain/better-readme) Copyright (c) 2024, [Nicola Brucoli](https://github.com/sonictrain).\n`
   }
 }
